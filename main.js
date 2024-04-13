0a259860043b721bb9db5b5460d4ba927b5ce0ca
AlcidesMa <alcidesma95@gmail.com>
2024-04-13 14:19:28 -0400

    funcionality finished

--- main.js
+++ main.js
@@ -6,23 +6,38 @@ const WORDS= [
 ]
 const table = document.getElementById('wordTable')
 const randomButton = document.getElementById('random-button')
+const resetButton = document.querySelector('#reset-button')
 const inputs = document.getElementsByTagName('input')
 function selectWord (){
   const randomIndex = Math.floor(Math.random()* 4)
   return WORDS[randomIndex]
 }
-const wordToReview = []
-const wordResponse = []
-
+let wordToReview = []
+let wordResponse = []
+let mistake= ''
+let tries = 5
 function setWordInGame(){
- 
+  setInputEvent()
+  setMistake()
+  setTries()
+  clearInputs()
+  wordResponse=[]
+  tries =5
   const word = selectWord()
+  wordToReview = word.split('')
+  randomizeWord(word)
+ 
 
+
+}
+
+
+function randomizeWord(word){
   const arr = word.split('').sort(() => Math.random() - 0.5)
   table.replaceChildren('')
   arr.map(
     ch => {
-      wordToReview.push(ch)
+    
       const liElement =document.createElement('li')
       liElement.textContent = ch
       table.append(liElement)
@@ -31,24 +46,80 @@ function setWordInGame(){
   )
 }
 
-setWordInGame();
-
-randomButton.addEventListener('click', setWordInGame)
+resetButton.addEventListener('click',setWordInGame)
+randomButton.addEventListener('click',()=> randomizeWord(wordToReview.join('')))
 
 function setInputEvent(){
+  
   for(let i =0 ; i < inputs.length; i++) {
     inputs[i].addEventListener('keyup',(event)=>{
-      wordResponse.push(event.key)
+     if(event.key ==='Backspace') return
+      wordResponse[i]=event.key
+      console.log(wordResponse)
       if(inputs[i+1]){
         inputs[i+1].focus()}
       else{
-        console.log(wordResponse)
+       const result = compareResult()
+       if(result.success ) {
+         alert('winner')
+         setWordInGame()
+       }
+        else { 
+          mistake = result.errors.join(',')
+          tries = tries-1
+          setMistake(mistake)
+          setTries(tries)
+          wordResponse=[]
+          if(tries<0) {
+            alert('juego terminado')
+            setWordInGame()
+          }
+        } 
+        inputs[0].focus()
       }
   })
 }
 }
 
+function compareResult(){
+  const errors= []
+  console.log({wordToReview})
+  wordToReview.forEach((element, index) => {
+    console.log({element})
+    console.log({wr:wordResponse[index]})
+    if(element!== wordResponse[index])
+    errors.push(wordResponse[index])
+  });
+  console.log(errors)
+  if(errors.length>0){
+    clearInputs()
+    return {
+      success:false,
+      errors
+    }
+  }
+  return {
+    success: true,
+    errors
+  }
+}
 
+function setMistake(errors=''){
+  const mistakeEl = document.querySelector('#mistake-board')
+  mistakeEl.replaceChildren('')
+  const liE=document.createElement('li')
+ liE.textContent = errors
+ mistakeEl.appendChild(liE)
+}
+function setTries(tries=5){
+  const triesSpan = document.querySelector('#tries-board')
+  triesSpan.textContent = tries
+}
 
-setInputEvent()
+function clearInputs(){
+  for(let i =0 ; i < inputs.length; i++){
+    inputs[i].value=''
+}
+}
+setWordInGame();
 
